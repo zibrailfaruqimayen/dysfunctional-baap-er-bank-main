2dcdb618c66724985b2d986d7e00a179d944d70f
zibrailfaruqimayen <zibrail.faruqi@gmail.com>
2023-03-08 03:12:24 +0600

    dysfunctional baper bank

--- _debug_steps.js
+++ _debug_steps.js
@@ -5,5 +5,12 @@ Debug Steps
 3. Read exception thoroughly in detail
 4. if the error is new to you : copy the text of the error and search online
 5. if a line number is showing , click on that line to see the code
-6. add breakpoint. re run the code (refresh  the website). and see whether you are getting the expected values
+6. add breakpoint. re run the code (refresh  the website). and see whether you  are getting the expected values
+     6.1: if you are not getting the expected value check which values you are  not  getting. if needed a break point in  the previous line
+     6.2: if needed ,step into (F12) to a function
+
+
+
+
+
 */
